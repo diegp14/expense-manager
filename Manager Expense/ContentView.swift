94142b63f69f66c16444aa4598e7df10701edcc2
Diego Guzman <diegp2001@gmail.com>
2025-10-19 15:59:20 -0600

    Initial Commit

---
+++ Manager Expense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Manager Expense
+//
+//  Created by Diego Guzman on 19/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Manager Expense/Manager_ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manager_ExpenseApp.swift
+//  Manager Expense
+//
+//  Created by Diego Guzman on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Manager_ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
