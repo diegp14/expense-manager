06f1f9e200a91bdeacd1ace5fdda2316bcdcd9cb
Diego <diegp2001@gmail.com>
2026-06-10 23:38:05 -0600

    ft: add options for filter to date

---
+++ ExpenseManager/Views/FilterDateView.swift
@@ -0,0 +1,18 @@
+//
+//  FilterDateView.swift
+//  ExpenseManager
+//
+//  Created by Diego Guzman on 10/06/26.
+//
+
+import SwiftUI
+
+struct FilterDateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterDateView()
+}
